28f056b9bf7c77c1d9a881920c302bf9e19a2f74
SachinthaVidanagamage <sachinthavida@gmail.com>
2025-12-25 09:14:38 +0530

    update from.cy.js file

--- cypress/e2e/demoQA/form.cy.js
+++ cypress/e2e/demoQA/form.cy.js
@@ -1,3 +1,7 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false
+})
+
 describe('Testing DemoQA Forms', () => {
 
   // Runs BEFORE each test
@@ -5,6 +9,16 @@ describe('Testing DemoQA Forms', () => {
     cy.visit('https://demoqa.com/automation-practice-form')
   }) 
   it('fills out and submits the form', () => {
+    //type first name and last name
+    // cy.get('input[id="firstName"]').type('Sachintha')
+    // cy.get('input[id="lastName"]').type('Vidanagamage')
+
+     cy.get('#firstName').type('Sachintha')
+    cy.get('#lastName').type('Vidanagamage')
+    //type email
+    cy.get('#userEmail').type('sachinthavida@gmail.com')
+
+    
 
   })    
 })   
