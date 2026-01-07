f58ab6286d92b3b9c07b50549ba91efd185e1625
SachinthaVidanagamage <sachinthavida@gmail.com>
2026-01-07 21:12:44 +0530

    testing demoQA  links

--- cypress/e2e/demoQA/links.cy.js
+++ cypress/e2e/demoQA/links.cy.js
@@ -1,12 +1,31 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false
+})
+
 describe('testing demoQA  links', () => {
     // Runs BEFORE each test
     beforeEach(() => {
       cy.visit('https://demoqa.com/links')
     })      
 
-    it('test home link', () => {
-      cy.get('#simpleLink').click()
-      cy.url().should('eq','https://demoqa.com/')
-      cy.go('back')
-    })
-     })
+    it('test simple link', () => {
+    cy.get('#simpleLink')
+      .invoke('removeAttr', 'target')
+      .click()
+
+    cy.url().should('eq', 'https://demoqa.com/')
+  })
+
+  it('test dynamic link', () => {    
+    cy.get('#dynamicLink')
+      .invoke('removeAttr', 'target')
+      .click()
+
+    cy.url().should('eq', 'https://demoqa.com/')
+  })
+
+
+
+})
+
+   
